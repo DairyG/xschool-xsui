5e32be7ad8b58f1bf137c02cd5365d150341bfaf
谭振华 <842926799@qq.com>
2019-06-19 17:38:42 +0800

    人员选择框

--- js/common.js
+++ js/common.js
@@ -1,3 +1,18 @@
+String.prototype.RTrim = function (c) {
+    if (!c) {
+        c = ' ';
+    }
+    var reg = new RegExp('([' + c + ']*$)', 'gi');
+    return this.replace(reg, '');
+}
+String.prototype.LTrim = function (c) {
+    if (!c) {
+        c = ' ';
+    }
+    var reg = new RegExp('(^[' + c + '])', 'gi');
+    return this.replace(reg, '');
+}
+
 /** 
  * 弹出输入层
  */
@@ -21,12 +36,12 @@ function layui_prompt(obj){
  * @param Object obj 显示选中内容的对象
  * @param boolean has_user 是否可选用户
  * @param boolean has_department 是否可选部门
- * @param boolean hsa_company 是否可选公司
+ * @param boolean has_company 是否可选公司
  * @param int num 最大选择数(大于零时限制选址数量，否者不限制)
  * @param boolean is_close_other 是否关闭其他弹窗
  * @param function callback 回调函数
  */
-function user_popup(obj = null,has_user = false,has_department = false,hsa_company = false,num = 0,is_close_other = false,callback = null){
+function user_popup(obj = null,has_user = false,has_department = false,has_company = false,num = 0,is_close_other = false,callback){
 	if(is_close_other){
 		layer.closeAll();
 	}
@@ -34,8 +49,14 @@ function user_popup(obj = null,has_user = false,has_department = false,hsa_compa
 	layui.use(['table'],function(){
 		table = layui.table;
 	});
-	$('body').append('<div id="popup_content"></div>');
-	$('#popup_content').load("../../pages/public/user_select2.html?has_user="+has_user+"&has_department="+has_department+"&hsa_company="+hsa_company+"&num="+num);
+	var user_company = {id:1,pid:0}//默认当前用户的公司信息 实际另行获取 
+	//是否可选公司单独再次判断  集团账户才能选择公司
+	if(user_company.pid > 0){
+		has_company = false;
+	}
+	
+	$('body').append('<div id="popup_content" data-has_user="'+has_user+'" data-has_department="'+has_department+'" data-has_company="'+has_company+'" data-num="'+num+'"></div>');
+	$('#popup_content').load("../../pages/public/user_select2.html");
 	
 	layer.open({
 		type: 1,
@@ -47,17 +68,48 @@ function user_popup(obj = null,has_user = false,has_department = false,hsa_compa
 		area: ['760px','480px'],
 		content: $('#popup_content'),
 		yes:function(index, layero){
-			var checkStatus = table.checkStatus('user_select_lst');
-			var values = $(obj).val(),ids = $(obj).attr('ids');
-			for (var i = 0; i < checkStatus.data.length; i++) {
-				var value = checkStatus.data[i].id;
-				$(obj).find('input').each(function(){
-					if($(this).val() == value || $(this).val() == 0){
-						sel_remove(this);
+			//以下方式可获取到选中的 公司 部门 人员
+			//注意去除
+			var company_ids = $("#selected_box #company_ids").val();//公司ID
+			var company_names = $("#selected_box #company_names").val();//公司名称
+			var department_ids = $("#selected_box #department_ids").val();//部门ID
+			var department_names = $("#selected_box #department_names").val();//部门名称
+			var user_ids = $("#selected_box #user_ids").val();//人员ID
+			var user_names = $("#selected_box #user_names").val();//人员名称
+			var arr = {
+				company:{
+					'ids':company_ids,
+					'names':company_names
+				},
+				department:{
+					'ids':department_ids,
+					'names':department_names
+				},
+				user:{
+					'ids':user_ids,
+					'names':user_names
+				}
+			};
+			if(typeof obj == 'object'){
+				if(has_department){
+					var html = "";
+					department_ids = department_ids.RTrim(',');
+					department_ids = department_ids.LTrim(',');
+					department_names = department_names.RTrim(',');
+					department_names = department_names.LTrim(',');
+					department_ids = department_ids ? department_ids.split(',') : [];
+					department_names = department_names ? department_names.split(',') : [];
+					if(department_names.length > 0){
+						for(var i = 0;i < department_names.length; i++){
+							html += build_sel_html('department',department_ids[i],department_names[i]);
+						}
 					}
-				});
-				var html = build_sel_html('user',value,checkStatus.data[i].name);
-				$(obj).append(html);
+					$(obj).html(html);
+				}
+			};
+			alert(typeof callback);
+			if(typeof callback === 'function'){
+				callback(arr);
 			}
 			layer.close(index);
 		},
@@ -90,6 +142,7 @@ function sel_remove(obj){
 //获取get参数 
 function GetPara(name) { 
     var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i"); 
+	console.log(window.location);
     var r = window.location.search.substr(1).match(reg); 
     if (r != null) 
         return unescape(r[2]); 
